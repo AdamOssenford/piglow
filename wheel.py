286749ff55aacf82bc83841a164bcb9e31d16daf
Adam Ossenford <adam.ossenford@drawbridge.com>
2016-05-03 00:14:51 -0500

    updated spacing

--- wheel.py
+++ wheel.py
@@ -5,17 +5,17 @@ from piglow import PiGlow
 piglow = PiGlow()
 
 for i in range(1 100):
-    #on
-    piglow.arm(0,100)
-    time.sleep(0.5)
-    piglow.arm(1,100)
-    time.sleep(0.5)
-    piglow.arm(2,100)
-    time.sleep(0.5)
-    #off
-    piglow.arm(0,0)
-    time.sleep(0.5)
-    piglow.arm(1,0)
-    time.sleep(0.5)
-    piglow.arm(2,0)
-    time.sleep(0.5)
+  #on
+  piglow.arm(0,100)
+  time.sleep(0.5)
+  piglow.arm(1,100)
+  time.sleep(0.5)
+  piglow.arm(2,100)
+  time.sleep(0.5)
+  #off
+  piglow.arm(0,0)
+  time.sleep(0.5)
+  piglow.arm(1,0)
+  time.sleep(0.5)
+  piglow.arm(2,0)
+  time.sleep(0.5)
