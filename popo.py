68f6d46728ae1d46614021c4c89aa457605317ae
Adam Ossenford <adam.ossenford@drawbridge.com>
2016-05-03 00:30:09 -0500

    meh

--- popo.py
+++ popo.py
@@ -3,9 +3,10 @@ from time import sleep
 from piglow import PiGlow
 
 piglow = PiGlow()
+piglow.all(0)
 # the police
 for i in range(1, 100):
   piglow.blue(100)
-  sleep(.1)
+  sleep(.2)
   piglow.red(100)
-  sleep(.1)
+  sleep(.2)
